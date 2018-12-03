692ec968ffce3d701eda553502b103a680c53ffa
bestlzl <121489727@qq.com>
2018-12-03 17:42:13 +0800

    项目搭建完成

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        组件
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      active: 0,
+      aaa: 0,
+      value:''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    font-size: 22px;
+  }
+</style>
